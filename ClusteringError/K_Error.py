a7b0b35c7eb9848bfe7d0245315f6b2118ce8e7c
Francesco Turini <fturini.turini7@gmail.com>
2024-02-29 16:58:22 +0100

    Creazione della struttra del pacchetto

---
+++ ClusteringError/K_Error.py
@@ -0,0 +1,7 @@
+class K_Error:
+    '''
+    Classe che utilizza un metodo modificato di K-means pesando gli errori.  
+    '''    
+    #Metodo di inizializzazione
+    def __init__(self,dati):
+        self.dati = dati
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ClusteringError',
+    readme = "README.md",
+    version = '0.dev0',
+    author='Francesco Turini',
+    author_email='fturini.turini7@gmail.com',
+    description='Libreria per fare il clustering di dati con errori di misura.',
+    url='https://github.com/fturini98/clustering-error-library',
+    license='GNU General Public License (GPL)',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+    ],
+)
